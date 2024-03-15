050de134833346aa71075b7a427a05b197d2e1eb
ahmed <ahmed.mooelsayed@gmail.com>
2024-03-15 13:39:48 +0200

    header banner

--- app/components/banner/Banner.tsx
+++ app/components/banner/Banner.tsx
@@ -1,58 +1,55 @@
-// import Image from 'next/image'
 import React from "react";
 import { Carousel } from "flowbite-react";
 import Image from "next/image";
 
+const banners = [
+  {
+    src: "/assets/images/banner-1.jpg",
+    alt: "women's latest fashion sale",
+    subtitle: "Trending item",
+    title: "Women's latest fashion sale",
+    text: "starting at $ 20.00",
+    btnText: "Shop now"
+  },
+  {
+    src: "/assets/images/banner-2.jpg",
+    alt: "modern sunglasses",
+    subtitle: "Trending accessories",
+    title: "Modern sunglasses",
+    text: "starting at $ 15.00",
+    btnText: "Shop now"
+  },
+  {
+    src: "/assets/images/banner-3.jpg",
+    alt: "new fashion summer sale",
+    subtitle: "Sale Offer",
+    title: "New fashion summer sale",
+    text: "starting at $ 29.99",
+    btnText: "Shop now"
+  }
+];
+
 const Banner = () => {
   return (
-    
-    <div className="m-auto w-11/12 h-96">
+    <div className="m-auto w-11/12 ">
       <Carousel rightControl=" " leftControl=" ">
-        <div className="relative">
-        <Image
-            src="/assets/images/banner-1.jpg"
-            alt="women's latest fashion sale"
-            className="banner-img object-contain"
-            width={1500}
-            height={500}
-          />
-          <div className="banner-content absolute top-1/2 left-20 -translate-y-1/2">
-            <p className="banner-subtitle">Trending item</p>
-            <h2 className="banner-title">Women s latest fashion sale</h2>
-            <p className="banner-text">starting at $ <b>20</b>.00</p>
-            <a href="#" className="banner-btn">Shop now</a>
+        {banners.map((banner, index) => (
+          <div className="relative h-[450px] aspect-w-auto aspect-h-auto rounded overflow-hidden" key={index}>
+            <Image
+              src={banner.src}
+              alt={banner.alt}
+              className="banner-img object-cover object-right p-0 m-0 block w-full h-full"
+              width={1500}
+              height={500}
+            />
+            <div className="banner-content absolute top-1/2 left-20 -translate-y-1/2">
+              <p className="banner-subtitle">{banner.subtitle}</p>
+              <h2 className="banner-title">{banner.title}</h2>
+              <p className="banner-text">{banner.text}</p>
+              <a href="#" className="banner-btn">{banner.btnText}</a>
+            </div>
           </div>
-        </div>
-        <div className="relative">
-        <Image
-            src="/assets/images/banner-2.jpg"
-            alt="modern sunglasses"
-            className="banner-img object-contain"
-            width={1500}
-            height={500}
-          />
-          <div className="banner-content absolute top-1/2 left-20 -translate-y-1/2">
-            <p className="banner-subtitle">Trending accessories</p>
-            <h2 className="banner-title">Modern sunglasses</h2>
-            <p className="banner-text">starting at $ <b>15</b>.00</p>
-            <a href="#" className="banner-btn">Shop now</a>
-          </div>
-        </div>
-        <div className="relative">
-        <Image
-            src="/assets/images/banner-3.jpg"
-            alt="new fashion summer sale"
-            className="banner-img object-contain"
-            width={1500}
-            height={500}
-          />
-          <div className="banner-content absolute top-1/2 left-20 -translate-y-1/2">
-            <p className="banner-subtitle">Sale Offer</p>
-            <h2 className="banner-title">New fashion summer sale</h2>
-            <p className="banner-text">starting at $ <b>29</b>.99</p>
-            <a href="#" className="banner-btn">Shop now</a>
-          </div>
-        </div>
+        ))}
       </Carousel>
     </div>
   );
