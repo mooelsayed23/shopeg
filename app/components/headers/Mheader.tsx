3089f0d3c2dc838c0ffe1c121dbfa59174db2392
ahmed <ahmed.mooelsayed@gmail.com>
2024-03-15 13:18:28 +0200

    header banner

--- app/components/headers/Mheader.tsx
+++ app/components/headers/Mheader.tsx
@@ -8,17 +8,17 @@ import { BiShoppingBag } from "react-icons/bi";
 const Mheader = () => {
   return (
     <>
-      <div className="header-main border-b sm:py-3">
+      <div className="header-main border-b sm:py-3 z-10">
         <div className="container flex flex-col sm:flex-row grow justify-evenly  items-center m-auto">
           <a href="#" className="header-logo m-3">
             <p className="logo text-3xl">ShopEg</p>
           </a>
 
-          <div className="header-search-container border rounded-md flex items-center px-4  ">
+          <div className="header-search-container border rounded-md flex items-center px-4  z-10">
             <input
               type="search"
               name="search"
-              className="search-field grow sm:min-w-72 py-2.5 outline-none"
+              className="search-field grow sm:min-w-72 py-2.5 outline-none border-none "
               placeholder="Enter your product name..."
             />
 
