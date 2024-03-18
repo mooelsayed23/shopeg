374800ee94226d77d7fb956fa4f38ed5c8657fa0
ahmed <ahmed.mooelsayed@gmail.com>
2024-03-19 00:28:56 +0200

    pixel footer

--- app/components/footer/Footernav.tsx
+++ app/components/footer/Footernav.tsx
@@ -27,11 +27,11 @@ const footerLists = [
   
   const Footer = () => {
     return (
-      <div className="flex mx-auto w-11/12 gap-5 justify-between border-b">
+      <div className="md:flex text-nowrap mx-auto w-11/12 gap-5 justify-between border-b">
         {footerLists.map((list, index) => (
           <ul className="footer-nav-list w-1/5 mb-10" key={index}>
             <li className="footer-nav-item relative">
-              <h2 className="nav-title font-bold text-xl text-white py-2 mt-8 after:bg-red-400 after:absolute after:bottom-0 after:left-0 after:h-px after:w-14 mb-6">{list.title}</h2>
+              <h2 className="nav-title font-bold text-xl text-white py-2 mt-8 after:bg-red-400 after:absolute after:bottom-0 after:left-0 after:h-px after:w-14 mb-6 text-wrap ">{list.title}</h2>
             </li>
             {list.links && list.links.map((link, idx) => (
               <li className="footer-nav-item" key={idx}>
@@ -41,7 +41,7 @@ const footerLists = [
             {list.info && (
               <li className="footer-nav-item flex items-start">
                 <div className="icon-box text-2xl mr-2"><IoLocationOutline/></div>
-                <address className="content text-sm">{list.info}</address>
+                <address className="content text-sm text-wrap ">{list.info}</address>
               </li>
             )}
             {list.phone && (
--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       </div>
       <Testimonial/>
       <Blog/>
-      {/* <Footerlayout/> */}
+      <Footerlayout/>
     </>
   );
 }
