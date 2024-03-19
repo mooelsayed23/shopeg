25b66838738f22d1f411503feb50f983fe60be3c
ahmed <ahmed.mooelsayed@gmail.com>
2024-03-19 14:48:22 +0200

    sidebar mob pixel

--- app/components/Sidbarmob/Footermob.tsx
+++ app/components/Sidbarmob/Footermob.tsx
@@ -13,14 +13,14 @@ import SidebarNav from "./SidebarNav";
 const Footermob = () => {
   const [hideside, setHideside] = useState(true);
   const [hidenav, setHideNave] = useState(true);
-  const opennav = ()=>{
-    setHideNave(!hidenav)
-    setHideside(true)
-  }
-  const openSide = ()=>{
-    setHideside(!hideside)
-    setHideNave(true)
-  }
+  const opennav = () => {
+    setHideNave(!hidenav);
+    setHideside(true);
+  };
+  const openSide = () => {
+    setHideside(!hideside);
+    setHideNave(true);
+  };
   return (
     <>
       <div className="fixed bottom-0 left-1/2 -translate-x-1/2 bg-white shadow-md shadow-gray-500  rounded-t-lg flex justify-evenly w-full sm:w-2/3 md:w-1/2 lg:hidden lg:w-0 text-gray-800 p-3 z-50 items-center">
@@ -51,7 +51,7 @@ const Footermob = () => {
         </button>
       </div>
       {hideside ? "" : <Sidebar />}
-      {hidenav ? "" :<SidebarNav />}
+      {hidenav ? "" : <SidebarNav />}
     </>
   );
 };
