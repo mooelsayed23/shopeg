5b297e7946d38756584e5145e1d8a500cb29bc86
ahmed <ahmed.mooelsayed@gmail.com>
2024-03-19 14:53:54 +0200

    sidebar mob pixel

--- app/components/footer/Footerbuy.tsx
+++ app/components/footer/Footerbuy.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footerbuy = () => {
   return (
     <>
-      <div className=" pt-5 mx-auto w-11/12 py-2">
+      <div className=" pt-5 mx-auto w-11/12 py-2 pb-16 lg:pb-2">
         <img
           src="./assets/images/payment.png"
           alt="payment method"
